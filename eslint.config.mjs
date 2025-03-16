3e6b857d8c916a29b2e8889f0f4289c92000e8b5
Francisco Mauri <labrest03@gmail.com>
2025-03-17 00:01:08 +0100

    Disable the Rule Globally in Your ESLint Configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,15 +2,22 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(
+    import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        // Add custom rules here
+        rules: {
+            "react/no-unescaped-entities": "off", // Disable the rule globally
+        },
+    },
 ];
 
 export default eslintConfig;
